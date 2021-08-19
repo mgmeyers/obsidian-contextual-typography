181afc979f7646147e9e0b1768d1b9d5e6015aac
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-08-19 10:15:50 -0700

    Account for print

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { Plugin } from "obsidian";
+import { MarkdownPostProcessorContext, Plugin } from "obsidian";
 
-function tagNode(node: Node) {
+function tagNode(node: Node, ctx: MarkdownPostProcessorContext) {
   if (node.nodeType === 3) {
     return;
   }
 
   const nodeEl = node as HTMLElement;
+  const isPrint = nodeEl.hasClass("markdown-preview-view");
 
   if (
+    !isPrint &&
     !nodeEl.dataset.tagName &&
     nodeEl.hasChildNodes() &&
     nodeEl.firstChild.nodeType !== 3
@@ -18,12 +20,30 @@ function tagNode(node: Node) {
       (k) => (nodeEl.dataset[k] = childEl.dataset[k])
     );
 
-    nodeEl.findAll('a.tag').forEach(tagEl => {
-      const tag = (tagEl as HTMLAnchorElement).innerText.slice(1).replace('/', '');
+    nodeEl.findAll("a.tag").forEach((tagEl) => {
+      const tag = (tagEl as HTMLAnchorElement).innerText
+        .slice(1)
+        .replace("/", "");
       nodeEl.addClass(`tag-${tag}`);
-    })
+    });
 
-    nodeEl.dataset.tagName = childEl.tagName.toLowerCase();
+    const tagName = childEl.tagName.toLowerCase();
+
+    nodeEl.dataset.tagName = tagName;
+    nodeEl.addClass(`el-${tagName}`);
+  } else if (isPrint && nodeEl.children.length > 0) {
+    const children = nodeEl.children;
+
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+
+      child.findAll("a.tag").forEach((tagEl) => {
+        const tag = (tagEl as HTMLAnchorElement).innerText
+          .slice(1)
+          .replace("/", "");
+        child.addClass(`tag-${tag}`);
+      });
+    }
   }
 }
 
